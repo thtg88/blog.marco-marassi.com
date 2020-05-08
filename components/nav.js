8ba17693b018ad0874f6091e6778bd0a2c146c8d
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-08 16:38:42 +0100

    Styling improvements

--- components/nav.js
+++ components/nav.js
@@ -2,21 +2,20 @@ import Link from 'next/link';
 
 const links = [
     { href: 'https://github.com/thtg88/blog.marco-marassi.com', label: 'Source' },
-    { href: 'https://github.com/thtg88', label: 'GitHub' },
     { href: 'https://www.marco-marassi.com', label: 'Website' },
 ];
 
 function Nav() {
     return (
         <nav>
-            <ul className="flex justify-between items-center p-8">
+            <ul className="flex justify-between items-center sm:p-8 p-4">
                 <li>
                     <Link href="/">
                         <a className="underline">Home</a>
                     </Link>
                 </li>
                 <li>
-                    <ul className="flex justify-between items-center space-x-4">
+                    <ul className="flex justify-between items-center sm:space-x-4 space-x-2">
                         {links.map(({ href, label }) => (
                             <li key={`${href}`}>
                             <a href={href} className="btn-black no-underline">
